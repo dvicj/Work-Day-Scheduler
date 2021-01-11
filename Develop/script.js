a2eee6d53a2a208c4d32e3e93ef9e95a9f9c3fb2
dvicjon <devinvjones@gmail.com>
2021-01-11 13:41:31 -0500

    updated styling

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,5 @@
 //create columns in bootstrap that are added with each time on the left and save button on the right
 //variables 
-var scheduleBox = document.querySelector(".schedule-box");
-var scheduleRow = document.querySelector(".row .schedule-row")
-var scheduleTime = document.querySelector(".schedule-time");
-var timeBlock = document.querySelector(".time-block");
-var scheduleTask = document.querySelector(".schedule.task");
 var currentDay = document.getElementById("currentDay");
 var currentTime = document.getElementById("currentTime");
 
@@ -75,33 +70,7 @@ var myDay = [
     }            
 ];
 
-//create array to hold tasks for saving
 var tasks = [];
-var taskTypeInput = $(".user-input").value;
-//save task as an object with time, status and push into task array 
-var taskDataObj = {
-  time: time, 
-  type: taskTypeInput
-}; 
-taskDataObj.index = taskTypeInput; 
-tasks.push(taskDataObj);
-//save tasks to localStorage 
-saveTasks(); 
-
-var saveTasks = function() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  console.log("tasks saved");
-};
-
-var loadTasks = function() {
-  var savedTasks = localStorage.getItem("tasks");
-  if (!savedTasks) {
-    return false; 
-  }
-  console.log("Saved tasks found!")
-  savedTasks = JSON.parse(savedTasks); 
-}
-
 
 //show current date 
 $('#currentDay').html("Today's date: " + moment().format("ddd. MMMM Do, YYYY"));
@@ -117,27 +86,6 @@ setInterval(function() {
   updateTime(); 
 },60000);
 
-//save data to local storage 
-function saveReminders() {
-  localStorage.setItem("myDay",JSON.stringify(myDay));
-}
-
-//sets any data in local storage to the view 
-function displayReminders() {
-  myDay.forEach(function(_thisHour) {
-    $(`#${_thisHour.index}`).val(_thisHour.reminder); 
-  })
-}
-
-function init() {
-  var storedDay = JSON.parse(localStorage.getItem("myDay"));
-  if (storedDay) {
-    myDay = storedDay; 
-  }
-  saveReminders(); 
-  displayReminders(); 
-}
-
 //schedule body 
 myDay.forEach(function(thisHour) {
   //creates time blocks 
@@ -146,10 +94,10 @@ myDay.forEach(function(thisHour) {
   //created time field
   var hourField = $("<div>")
   .text(`${thisHour.hour}${thisHour.minute}`) //create text field with hour and am/pm
-  .attr({"class": "col-md-2 hour"}); // add column classes 
+  .attr({"class": "col-2 hour"}); // add column classes 
   //creates schedule data 
   var hourPlan = $("<div>")
-  .attr({"class":"col-md-9 description p-0"}); //create div add column classes 
+  .attr({"class":"col-9 description"}); //create div add column classes 
   var planData = $("<textarea>").attr({"class":"user-input"}); //create text area 
   hourPlan.append(planData); // add hour div to text area 
   planData.attr("index", thisHour.index); //add id to text area 
@@ -161,25 +109,29 @@ myDay.forEach(function(thisHour) {
     planData.attr({"class":"future"})
   }
   //create save button 
-  var saveButton = $("<i class='far fa-save fa-lg></i>") //create save button icon 
-  var savePlan = $("<button>").attr({"class": "col-md-1 saveBtn"}); //creat save button 
+  var saveButton = $("<i class='far fa-save'></i>") //create save button icon 
+  var savePlan = $("<button>").attr({"class": "col-1 saveBtn"}); //creat save button 
   savePlan.append(saveButton); //add button to icon 
   hourRow.append(hourField, hourPlan, savePlan); //add time and save button to row 
 });
 
-//loads any existing localstorage data after components created 
-init(); 
-
 //saves data to be used in localStorage 
 $(".saveBtn").on("click", function(event) {
   event.preventDefault(); 
-  var saveTextInput = $("user-input"); 
-  localStorage.setItem("saveTextInput", JSON.stringify(saveTextInput));
-  console.log(saveTextInput + "saved"); 
-  //var saveIndex = $(this).siblings(".description").children(".future").attr("index");
- // myDay[saveIndex].reminder = $(this).siblings(".description").children(".future").val(); 
- // console.log(saveIndex);
-  saveReminders(); 
-  displayReminders(); 
+  saveTasks(); 
+  loadTasks(); 
 })
+var saveTextInput = $("tasks");
+var saveTasks = function() {
+  localStorage.setItem("saveTextInput", JSON.stringify(saveTextInput));
+  console.log ("tasks saved");
+}
+var loadTasks = function() {
+  var savedTasks = localStorage.getItem("saveTextInput");
+  //if there are no tasks to an empty array 
+  if (!savedTasks) {
+    return false; 
+  }
+  savedTasks = JSON.parse(savedTasks);
+}
 
